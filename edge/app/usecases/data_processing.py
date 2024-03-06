114a15a997953fd4fdd7da26c8d830e5bfe995bd
Vladlen <ilusenkovladlen@gmail.com>
2024-03-06 18:10:59 +0200

    implemented process_agent_data

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,3 +12,14 @@ agent_data: AgentData,
     processed_data (ProcessedAgentData): Processed data containing the classified state of
     the road surface and agent data.
     """
+    z_value = agent_data.accelerometer.z
+
+    if z_value < 16541 - 1500:
+        road_state = "bump"
+    elif z_value > 16541 + 1500:
+        road_state = "pothole"
+    else:
+        road_state = "normal"
+
+    processed_data = ProcessedAgentData(road_state=road_state, agent_data=agent_data)
+    return processed_data
